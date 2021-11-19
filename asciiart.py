b4ec16c10ed994a05461fc393bcaae81d07fcc76
mmr <77846862+gosia1138@users.noreply.github.com>
2021-11-19 15:24:22 +0100

    Add ASCII art to represent doors
    ASCII art source: The Monty Hall Problem, by Al Sweigart al@inventwithpython.com
    View this code at https://nostarch.com/big-book-small-python-projects

---
+++ asciiart.py
@@ -0,0 +1,77 @@
+'''This ASCII art was created by Al Sweigart as part of his MontyHall
+python script.
+https://nostarch.com/big-book-small-python-projects'''
+
+ALL_CLOSED = """
+ +------+  +------+  +------+
+ |      |  |      |  |      |
+ |   1  |  |   2  |  |   3  |
+ |      |  |      |  |      |
+ |      |  |      |  |      |
+ |      |  |      |  |      |
+ +------+  +------+  +------+"""
+
+FIRST_GOAT = """
+ +------+  +------+  +------+
+ |  ((  |  |      |  |      |
+ |  oo  |  |   2  |  |   3  |
+ | /_/|_|  |      |  |      |
+ |    | |  |      |  |      |
+ |GOAT|||  |      |  |      |
+ +------+  +------+  +------+"""
+
+SECOND_GOAT = """
+ +------+  +------+  +------+
+ |      |  |  ((  |  |      |
+ |   1  |  |  oo  |  |   3  |
+ |      |  | /_/|_|  |      |
+ |      |  |    | |  |      |
+ |      |  |GOAT|||  |      |
+ +------+  +------+  +------+"""
+
+THIRD_GOAT = """
+ +------+  +------+  +------+
+ |      |  |      |  |  ((  |
+ |   1  |  |   2  |  |  oo  |
+ |      |  |      |  | /_/|_|
+ |      |  |      |  |    | |
+ |      |  |      |  |GOAT|||
+ +------+  +------+  +------+"""
+
+FIRST_CAR_OTHERS_GOAT = """
+ +------+  +------+  +------+
+ | CAR! |  |  ((  |  |  ((  |
+ |    __|  |  oo  |  |  oo  |
+ |  _/  |  | /_/|_|  | /_/|_|
+ | /_ __|  |    | |  |    | |
+ |   O  |  |GOAT|||  |GOAT|||
+ +------+  +------+  +------+"""
+
+SECOND_CAR_OTHERS_GOAT = """
+ +------+  +------+  +------+
+ |  ((  |  | CAR! |  |  ((  |
+ |  oo  |  |    __|  |  oo  |
+ | /_/|_|  |  _/  |  | /_/|_|
+ |    | |  | /_ __|  |    | |
+ |GOAT|||  |   O  |  |GOAT|||
+ +------+  +------+  +------+"""
+
+THIRD_CAR_OTHERS_GOAT = """
+ +------+  +------+  +------+
+ |  ((  |  |  ((  |  | CAR! |
+ |  oo  |  |  oo  |  |    __|
+ | /_/|_|  | /_/|_|  |  _/  |
+ |    | |  |    | |  | /_ __|
+ |GOAT|||  |GOAT|||  |   O  |
+ +------+  +------+  +------+"""
+
+
+DOOR_ART = {
+    "all_closed": ALL_CLOSED,
+    "goat1": FIRST_GOAT,
+    "goat2": SECOND_GOAT,
+    "goat3": THIRD_GOAT,
+    "car1": FIRST_CAR_OTHERS_GOAT,
+    "car2": SECOND_CAR_OTHERS_GOAT,
+    "car3": THIRD_CAR_OTHERS_GOAT
+}
--- monty.py
+++ monty.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+from asciiart import DOOR_ART
 
 
 def prepare_doors(door_count):
@@ -18,7 +19,7 @@ def users_choice(door_count):
     """gathers users input and checks if valid"""
     uc = 0
     while True:
-        uc = input("Which door do you want to open? Enter 1, 2 or 3: ")
+        uc = input("\nWhich door do you want to open? Enter 1, 2 or 3: ")
         if uc.isnumeric():
             if int(uc) <= door_count:
                 return int(uc)
@@ -34,48 +35,49 @@ Welcome to the Monty Hall Simulator!
 
 You are presented with three doors.
 Behind two of them are goats, but behind one of them is a car!
-Choose the door and take home the prize behind it!
- ___   ___   ___
-|   | |   | |   |
-| 1 | | 2 | | 3 |
-|___| |___| |___|
-    """)
-
+Choose the door and take home the prize behind it!""")
+    print(DOOR_ART["all_closed"])
     doors = prepare_doors(door_count)
     first_choice = users_choice(door_count)
     alternatives = list(doors.keys())
     alternatives.remove(first_choice)
-    # Finds the other door with goat behind to present to the player
+    # Finds the other door with goat behind to present to the player and gets
+    # the car door number to use in the final reveal.
     goat_doors = []
+    car_door = -1
     for k, v in doors.items():
-        if k == first_choice or v == "car":
+        if v == "car":
+            car_door = k
+        if k == first_choice:
             continue
         else:
             goat_doors.append(k)
     goat_door = random.choice(goat_doors)
     alternatives.remove(goat_door)
     alt_door = alternatives[0]
-    print("You have chosen door number {}".format(first_choice))
-    print("What if I told you that there is a goat behind dooe number {}?".format(
+    print("\nYour choice: door number {}".format(first_choice))
+    print("\nWhat if I told you that there is a goat behind door number {}?".format(
         str(goat_door)))
-    print("Do you want to stick with door number {} or prefer to switch to door number {}?".format(
+    print(DOOR_ART["goat{}".format(goat_door)])
+    print("Do you want to stick with door number {} or prefer to swap for door number {}?".format(
         str(first_choice), str(alt_door)))
     # Player can now choose to open the other remaining door or to stick with
     # their original choice of door
     while True:
-        final_choice = input("Enter the door number: ")
-        if final_choice.isnumeric():
-            final_choice = int(final_choice)
-            if final_choice in (first_choice, alt_door):
-                break
-        else:
-            print("You can only choose between door number {} or {}".format(
-                str(first_choice), str(alt_door)))
+        final_choice = input("Do you want to swap? (y/n)")
+        if final_choice in ['y', 'Y']:
+            final_choice = alt_door
+            break
+        if final_choice in ['n', 'N']:
+            final_choice = first_choice
+            break
     # Revealing the players prize
     if doors[final_choice] == "car":
-        print("Congratulations! You just won a car!")
+        print(DOOR_ART["car{}".format(car_door)])
+        print("\nCongratulations! You just won a car!")
     else:
-        print("Congratulations! You just won a goat!")
+        print(DOOR_ART["car{}".format(car_door)])
+        print("\nCongratulations! You just won a goat!")
 
 
 if __name__ == "__main__":
