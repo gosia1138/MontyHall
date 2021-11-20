65f8aa6062fa08053f7ef3ab10748042b8b5ace7
mmr <77846862+gosia1138@users.noreply.github.com>
2021-11-20 12:02:40 +0100

    code refactoring

--- monty.py
+++ monty.py
@@ -3,33 +3,7 @@ import random
 from asciiart import DOOR_ART
 
 
-def prepare_doors(door_count):
-    """Build up the doors, herd the goats, hide the car"""
-    car_door = random.randint(1, door_count)
-    doors = {}
-    for door in range(1, door_count + 1):
-        if door == car_door:
-            doors[door] = "car"
-        else:
-            doors[door] = "goat"
-    return doors
-
-
-def users_choice(door_count):
-    """gathers users input and checks if valid"""
-    uc = 0
-    while True:
-        uc = input("\nWhich door do you want to open? Enter 1, 2 or 3: ")
-        if uc.isnumeric():
-            if int(uc) <= door_count:
-                return int(uc)
-            else:
-                print("Thats more than there is doors!")
-        elif not uc.isnumeric():
-            print("Oh cmon, that is not even a number!")
-
-
-def main(door_count):
+def main():
     print("""
 Welcome to the Monty Hall Simulator!
 
@@ -37,42 +11,41 @@ You are presented with three doors.
 Behind two of them are goats, but behind one of them is a car!
 Choose the door and take home the prize behind it!""")
     print(DOOR_ART["all_closed"])
-    doors = prepare_doors(door_count)
-    first_choice = users_choice(door_count)
-    alternatives = list(doors.keys())
-    alternatives.remove(first_choice)
-    # Finds the other door with goat behind to present to the player and gets
-    # the car door number to use in the final reveal.
-    goat_doors = []
-    car_door = -1
-    for k, v in doors.items():
-        if v == "car":
-            car_door = k
-        if k == first_choice:
-            continue
-        else:
-            goat_doors.append(k)
-    goat_door = random.choice(goat_doors)
-    alternatives.remove(goat_door)
-    alt_door = alternatives[0]
-    print("\nYour choice: door number {}".format(first_choice))
+
+    # generate car door
+    car_door = random.randint(1, 3)
+
+    while True:  # prompt user for door number
+        print("Please choose door number 1, 2 or 3:")
+        first_choice = input("> ")
+        if first_choice == "1" or first_choice == "2" or first_choice == "3":
+            break
+
+    while True:  # find door with a goat to present to user
+        goat_door = str(random.randint(1, 3))
+        if goat_door != car_door and goat_door != first_choice:
+            break
+
+    alt_door = str(6 - int(goat_door) - int(first_choice))  # swap door
+
+    # Presenting goat door and prompting user if want to swap
     print("\nWhat if I told you that there is a goat behind door number {}?".format(
         str(goat_door)))
     print(DOOR_ART["goat{}".format(goat_door)])
-    print("Do you want to stick with door number {} or prefer to swap for door number {}?".format(
+    print("Do you still want to open door number {} or swap for door number {}?".format(
         str(first_choice), str(alt_door)))
-    # Player can now choose to open the other remaining door or to stick with
-    # their original choice of door
-    while True:
-        final_choice = input("Do you want to swap? (y/n)")
-        if final_choice in ['y', 'Y']:
+
+    while True:  # users choice to swap or stick
+        decision = input("Do you want to swap? (y/n)")
+        if decision in ['y', 'Y']:
             final_choice = alt_door
             break
         if final_choice in ['n', 'N']:
             final_choice = first_choice
             break
-    # Revealing the players prize
-    if doors[final_choice] == "car":
+
+    # Revealing the prize
+    if final_choice == car_door:
         print(DOOR_ART["car{}".format(car_door)])
         print("\nCongratulations! You just won a car!")
     else:
@@ -81,5 +54,4 @@ Choose the door and take home the prize behind it!""")
 
 
 if __name__ == "__main__":
-    door_count = 3
-    main(door_count)
+    main()
